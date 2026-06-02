7030c7df44bb3ad2fe0c4ee926dbe0fac4657a16
Henry Ventura <henry.ventura@tecsup.edu.pe.com>
2026-06-02 13:59:24 -0500

    Controlador MedicalRecord creado

---
+++ Veterinaria/Controllers/MedicalRecordController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using VetClinic.Application.UseCases.MedicalRecord.Commands;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Veterinaria.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class MedicalRecordController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public MedicalRecordController(IMediator mediator) => _mediator = mediator;
+
+    [HttpPost]
+    [Authorize(Roles = "Administrador,Veterinario")]
+    public async Task<IActionResult> Add([FromBody] AddMedicalRecordCommand request)
+    {
+        await _mediator.Send(request);
+        return Ok("Registro médico añadido");
+    }
+}
