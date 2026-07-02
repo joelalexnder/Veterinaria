1b28b42ea9ad6779298e8ab1f54132eebb4f5521
albieri765 <albieriaqperu@gmail.com>
2026-07-02 09:41:50 -0500

    Resolviendo ownermodels

--- VetClinic.Application/UseCases/Owner/Commands/RegisterOwnerCommand.cs
+++ VetClinic.Application/UseCases/Owner/Commands/RegisterOwnerCommand.cs
@@ -8,12 +8,24 @@ namespace VetClinic.Application.UseCases.Owner.Commands;
 public class RegisterOwnerCommand : IRequest<Unit>
 {
     [Required(ErrorMessage = "El nombre completo es obligatorio")]
-    [RegularExpression(@"^(?=.*[a-zA-ZÀ-ÿ]).+$", ErrorMessage = "El nombre debe contener al menos una letra")]
+    [RegularExpression(@"^(?=.*[a-zA-ZÀ-ÿ]).+$",
+        ErrorMessage = "El nombre debe contener al menos una letra")]
     public string FullName { get; set; } = null!;
 
     [Required(ErrorMessage = "El DNI es obligatorio")]
+    [StringLength(8,
+        MinimumLength = 8,
+        ErrorMessage = "El DNI debe tener exactamente 8 dígitos")]
+    [RegularExpression(@"^\d{8}$",
+        ErrorMessage = "El DNI solo debe contener números")]
     public string Dni { get; set; } = null!;
 
+    [Required(ErrorMessage = "El teléfono es obligatorio")]
+    [StringLength(9,
+        MinimumLength = 9,
+        ErrorMessage = "El teléfono debe tener 9 dígitos")]
+    [RegularExpression(@"^\d{9}$",
+        ErrorMessage = "El teléfono solo debe contener números")]
     public string? Phone { get; set; }
 
     [EmailAddress(ErrorMessage = "El email no tiene un formato válido")]
@@ -43,7 +55,4 @@ public class RegisterOwnerCommandHandler : IRequestHandler<RegisterOwnerCommand,
 
         return Unit.Value;
     }
-    [Required(ErrorMessage = "El nombre completo es obligatorio")]
-[RegularExpression(@"^(?=.*[a-zA-ZÀ-ÿ]).+$", ErrorMessage = "El nombre debe contener al menos una letra")]
-public string FullName { get; set; } = null!;
 }
--- VetClinic.Web/Models/OwnerModels.cs
+++ VetClinic.Web/Models/OwnerModels.cs
@@ -16,12 +16,24 @@ public class OwnerDto
 public class RegisterOwnerRequest
 {
     [Required(ErrorMessage = "El nombre completo es obligatorio")]
-    [RegularExpression(@"^(?=.*[a-zA-ZÀ-ÿ]).+$", ErrorMessage = "El nombre debe contener al menos una letra")]
+    [RegularExpression(@"^(?=.*[a-zA-ZÀ-ÿ]).+$",
+        ErrorMessage = "El nombre debe contener al menos una letra")]
     public string FullName { get; set; } = "";
 
     [Required(ErrorMessage = "El DNI es obligatorio")]
+    [StringLength(8,
+        MinimumLength = 8,
+        ErrorMessage = "El DNI debe tener exactamente 8 dígitos")]
+    [RegularExpression(@"^\d{8}$",
+        ErrorMessage = "El DNI solo debe contener números")]
     public string Dni { get; set; } = "";
 
+    [Required(ErrorMessage = "El teléfono es obligatorio")]
+    [StringLength(9,
+        MinimumLength = 9,
+        ErrorMessage = "El teléfono debe tener 9 dígitos")]
+    [RegularExpression(@"^\d{9}$",
+        ErrorMessage = "El teléfono solo debe contener números")]
     public string? Phone { get; set; }
 
     [EmailAddress(ErrorMessage = "Email inválido")]
